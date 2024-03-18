2fa781e6537a8e8ade9c9f984b16d9cf00b27d0a
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-03-18 15:12:42 -0500

    the gallery works

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Marathon Running</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Its a great way to stay in shape.</li>
+            <li>You get to spend several hours and 26 miles exploring the city.</li>
+            <li>It gives you plenty of opportunity to improve on your performance</li>
          </ol>
       </div>
    );
