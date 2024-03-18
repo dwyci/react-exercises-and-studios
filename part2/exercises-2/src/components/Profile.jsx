4529a1cb136d3691287d1706c926ff65b2d4427c
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-03-18 11:32:33 -0500

    cocmpleted exercise 2

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,35 @@
 import './styles.css';
 import Button from './Button';
 
+import Oceans from './oceans.json'; 
+
+const listItem = Oceans.map(ocean =>
+   <div key={ocean.id}  className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <img src= {ocean.image} alt= {ocean.name} className='img'/>
+      <h1>{ocean.name} </h1>
+      <h3>Fun Facts!</h3>
+      <ol>
+         <li>{ocean.fact1}</li>
+         <li>{ocean.fact2}</li>
+         <li>{ocean.fact3}</li>
+
+      </ol>
+      
+      <Button />
+
+   </div>
+   );
+
 
 function Profile()
 {
+  
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+     
+      <ul>
+         {listItem}
+      </ul>
+   
    );
 }
 
